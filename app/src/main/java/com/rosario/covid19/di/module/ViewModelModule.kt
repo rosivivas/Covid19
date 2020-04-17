3fe859e2b74fb4d2af0c04e467dce23045a2258d
Rosario Vivas <rosario@finvox.com>
2020-04-17 11:41:31 -0400

    handle empty response

--- app/src/main/java/com/rosario/covid19/di/module/ViewModelModule.kt
+++ app/src/main/java/com/rosario/covid19/di/module/ViewModelModule.kt
@@ -1,5 +1,6 @@
 package com.rosario.covid19.di.module
 
+import android.content.Context
 import com.rosario.covid19.domain.ReportUseCase
 import com.rosario.covid19.viewModel.HomeViewModel
 import dagger.Module
@@ -11,5 +12,5 @@ import dagger.Provides
 @Module
 class ViewModelModule {
     @Provides
-    fun homeViewModel(useCase: ReportUseCase) = HomeViewModel(useCase)
+    fun homeViewModel(useCase: ReportUseCase, context: Context) = HomeViewModel(useCase, context)
 }
--- app/src/main/java/com/rosario/covid19/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/rosario/covid19/viewModel/HomeViewModel.kt
@@ -1,7 +1,10 @@
 package com.rosario.covid19.viewModel
 
+import android.content.Context
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.MutableLiveData
+import com.rosario.covid19.R
 import com.rosario.covid19.data.model.DataResponse
 import com.rosario.covid19.data.model.Report
 import com.rosario.covid19.domain.ReportUseCase
@@ -13,13 +16,19 @@ import javax.inject.Inject
 /**
  * Handle connection to server and show data to UI
  */
-class HomeViewModel @Inject constructor(private val reportUseCase: ReportUseCase) {
+class HomeViewModel @Inject constructor(
+    private val reportUseCase: ReportUseCase,
+    private val context: Context
+) {
 
     private lateinit var subscription: Disposable
     var response = MutableLiveData<Report>()
     var error = MutableLiveData<Throwable>()
     var progressBar = MutableLiveData<Int>().apply { postValue(View.GONE) }
     var dataVisibility = MutableLiveData<Int>().apply { postValue(View.GONE) }
+    var confirmed = MutableLiveData<String>().apply { postValue("") }
+    var deaths = MutableLiveData<String>().apply { postValue("") }
+
 
     /**
      *  Get data from server
@@ -53,7 +62,38 @@ class HomeViewModel @Inject constructor(private val reportUseCase: ReportUseCase
      *  Get success response data
      */
     private fun registerSuccess(result: DataResponse?) {
-        response.value = result!!.data
+        if (result!!.data.size > 0) {
+            if (result.data.containsKey("confirmed")) {
+                val dataConfirmed  = result.data["confirmed"] as Double
+                val dataDeaths = result.data["deaths"] as Double
+
+                confirmed.postValue(
+                    String.format(
+                        context.resources.getString(R.string.cases_confirmed),
+                        dataConfirmed.toInt()
+                    )
+                )
+                deaths.postValue(
+                    String.format(
+                        context.resources.getString(R.string.deaths_confirmed),
+                        dataDeaths.toInt()
+                    )
+                )
+            }
+        } else {
+            confirmed.postValue(
+                String.format(
+                    context.resources.getString(R.string.cases_confirmed),
+                    0
+                )
+            )
+            deaths.postValue(
+                String.format(
+                    context.resources.getString(R.string.deaths_confirmed),
+                    0
+                )
+            )
+        }
         dataVisibility.postValue(View.VISIBLE)
     }
 
